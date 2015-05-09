b904a47024ebab7571be1a4f4e1d839e7b5b724f
Kyle Lewis <thekyle@gmail.com>
2015-05-09 14:00:55 -0400

    Separating out learning tests

---
+++ lib/D2L.Extensibility.AuthSdk.IntegrationTests/LearningTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using D2L.Extensibility.AuthSdk.IntegrationTests.Helpers;
+using D2L.Extensibility.AuthSdk.IntegrationTests.Providers;
+using NUnit.Framework;
+
+namespace D2L.Extensibility.AuthSdk.IntegrationTests {
+
+    [TestFixture]
+    public class LearningTests {
+
+        [Test]
+        public void LearningTest_SendRequestWithBadKeys_ThrowsWebException() {
+            var request = RequestProvider.PrepareApiRequest( ContextProvider.BadUserContext() , RouteProvider.OrganizationInfoRoute );
+
+            Assert.Throws<WebException>( () => request.GetResponse() );
+        }
+
+        [Test]
+        public void LearningTest_SendRequestWithBadKeys_ResponseBodyContains_Invalid_token() {
+            var request = RequestProvider.PrepareApiRequest( ContextProvider.BadUserContext(), RouteProvider.OrganizationInfoRoute );
+
+            try {
+                request.GetResponse();
+            } catch ( WebException ex ) {
+                var responseBody = StringHelper.ReadResponseContents( ex.Response as HttpWebResponse );
+                Assert.IsTrue( responseBody.Equals( "Invalid token", StringComparison.InvariantCulture ) );
+            }
+        }
+    }
+}
